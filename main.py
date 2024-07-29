ea6392a2c96afd5ec87f3069a0c6f581ab0d4de3
Renat Shabanov <multizloy@gmail.com>
2024-07-29 11:39:37 +0300

    Battleship start

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+from settings import *
+
+pygame.init()
+
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Battleship")
---
+++ settings.py
@@ -0,0 +1,8 @@
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+blockSize = 30
+leftMargin = 40
+upperMargin = 50
+
+size = (leftMargin + 30 * blockSize, upperMargin + 15 * blockSize)
