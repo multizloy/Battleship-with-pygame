a6958ea1b1ef1d7f97e4d8619faee0c690a1e185
Renat Shabanov <multizloy@gmail.com>
2024-08-04 10:04:47 +0300

    .

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ pygame.init()
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Battleship")
+
+fontSize = int(blockSize // 1.5)
+
+font = pygame.font.SysFont("notosans", fontSize)
+
+
+def main():
+    gameOver = False
+    screen.fill(WHITE)
+    while not gameOver:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameOver = True
+
+        pygame.display.update()
+
+
+main()
+pygame.quit()
